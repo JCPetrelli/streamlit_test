1f9255f0e1c417b1cd42fcbfa6931ffa2a78530a
Jacopo Castellano <jacopocastellano87@gmail.com>
2023-04-25 15:43:56 +0200

    Initial Commit

---
+++ start.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write("""
+## References
+1. https://levelup.gitconnected.com/how-to-create-a-doc-chatbot-that-learns-everything-for-you-in-15-minutes-364fef481307
+2. https://docs.streamlit.io/library/api-reference
+""")
